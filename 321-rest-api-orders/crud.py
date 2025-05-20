d12fa5d3d22dd205957bb6204793067228e534fb
PedroSilva02 <pedro02school@gmail.com>
2025-05-20 20:17:56 +0200

    Add http request from fastapi to node

--- 321-rest-api-orders/crud.py
+++ 321-rest-api-orders/crud.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
 import models, schemas
+import requests
+from fastapi import HTTPException
 
 def create_commande_with_produits(db: Session, commande: schemas.CommandeCreate):
     total = 0
@@ -8,13 +10,20 @@ def create_commande_with_produits(db: Session, commande: schemas.CommandeCreate)
     db.flush()
 
     for item in commande.produits:
+        # 🔍 Vérifie le produit auprès du service Node
+        res = requests.get(f"http://produits-service:8000/api/produits/{item.produit_id}")
+        if res.status_code != 200:
+            raise HTTPException(status_code=404, detail=f"Produit ID {item.produit_id} non trouvé")
+
+        produit = res.json()
+        total += produit["prix"] * item.quantite
+
         cp = models.CommandeProduit(
             commande_id=db_commande.id,
             produit_id=item.produit_id,
             quantite=item.quantite
         )
         db.add(cp)
-        total += 10 * item.quantite  # prix fictif
 
     db_commande.total = total
     db.commit()
